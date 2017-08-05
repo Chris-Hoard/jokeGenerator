2e4f1265f445bb2a2388db661c434ecb0e2bb5a4
Chris Hoard <hoard.chris@gmail.com>
2017-08-05 15:41:30 +0000

    Added Tweet button

--- jokeGeneratorAPI.js
+++ jokeGeneratorAPI.js
@@ -1,22 +1,30 @@
 /* global $ */
 
-//Logic to return a Church Norris Joke from the API
-function getJoke(){
-  var url = "https://api.chucknorris.io/jokes/random";
-  $.ajax({
-    url: url,
-    success: function(response) {
-        console.log(response);
-          console.log(response.quote);
-           $('#joke-text').text(response.value);
-       return response.value;
-  
-    },
-    dataType: "json"
+$(document).ready(function() {
+   
+   $("#jokeGenerator").submit(function (event){
+     event.preventDefault();
+     var url = "https://api.chucknorris.io/jokes/random";
+
+     $.ajax({
+            url: url,
+            //data: data,
+            type: "GET",
+            success: function(response) {
+                //console.log(response);
+                var html = "<h1 id='jokeText' class='well'>"+response.value +"</h1>";
+                console.log(html);
+                $("#jokeHolder").html(html);
+                var href = $('#tweetLink').attr('href');
+                $('#tweetLink').attr('href',href +response.value );
+
+              
+            }
+        });
   });
-  
-}
-//On initial initalization, display the first Quote
-getJoke();
+});
+
+
+
 
  
